9948e26fca088f6e5e521820646dbe2c15329907
Sehrishah <sehrishwaheed98@gmail.com>
2020-10-27 23:53:26 +0300

    Straight Flush rank and Royal Flush rank

--- poker/hand.py
+++ poker/hand.py
@@ -9,6 +9,10 @@ class Hand():
     @property
     def _rank_validation_from_best_to_worst(self):
         return (
+            ("Royal Flush", self._royal_flush),
+            ("Straight Flush", self._straight_flush),
+            ("Four of a Kind", self._four_of_a_kind),
+            ("Full House", self._full_house),
             ("Flush", self._flush),
             ("Straight", self._straight),
             ("Three of a Kind", self._three_of_a_kind),
@@ -24,6 +28,23 @@ class Hand():
             name, validator_func = rank
             if validator_func():
                 return name
+
+    def _royal_flush(self):
+        is_straight_flush = self._straight_flush()
+        is_royal = self.cards[-1].rank == "Ace" 
+        return is_straight_flush and is_royal           
+
+    def _straight_flush(self):
+        return self._straight() and self._flush()
+
+
+    def _four_of_a_kind(self):
+        # in texas hold em poker we only have 7 cards two for us and seven for community
+        rank_with_four_of_a_kind = self._ranks_with_count(4)
+        return len(rank_with_four_of_a_kind) == 1                    
+
+    def _full_house(self):
+        return self._three_of_a_kind() and self._pair()        
 
     def _flush(self):
         suits_that_occur_5_or_more_times = {
--- tests/test_hand.py
+++ tests/test_hand.py
@@ -115,6 +115,62 @@ class HandTest(unittest.TestCase):
             "Flush"
         )
 
+    def test_figures_out_full_house_is_best_rank(self):
+        cards = [
+            Card(rank = "3", suit = "Clubs"),
+            Card(rank = "3", suit = "Hearts"),
+            Card(rank = "3", suit = "Spades"),
+            Card(rank = "9", suit = "Diamonds"),
+            Card(rank = "9", suit = "Spades")
+        ]
+        hand = Hand(cards)
+
+        self.assertEqual(
+            hand.best_rank(),
+            "Full House"
+        )
+    def test_figures_out_four_of_a_kind_is_a_best_rank(self):
+        cards = [
+            Card(rank = "3", suit = "Clubs"),
+            Card(rank = "3", suit = "Hearts"),
+            Card(rank = "3", suit = "Spades"),
+            Card(rank = "3", suit = "Diamonds"),
+            Card(rank = "9", suit = "Spades")
+        ]
+        hand = Hand(cards = cards)
+        self.assertEqual(
+            hand.best_rank(),
+            "Four of a Kind"
+        )
+
+    def test_figures_out_straight_flush_is_best_rank(self):
+        cards = [
+            Card(rank = "3", suit = "Clubs"),
+            Card(rank = "4", suit = "Clubs"),
+            Card(rank = "5", suit = "Clubs"),
+            Card(rank = "6", suit = "Clubs"),
+            Card(rank = "7", suit = "Clubs")
+        ] 
+
+        hand = Hand(cards = cards) 
+        self.assertEqual(
+            hand.best_rank(),
+            "Straight Flush"
+        )
+
+    def test_figures_out_straight_flush_is_best_rank(self):
+        cards = [
+            Card(rank = "10", suit = "Clubs"),
+            Card(rank = "Jack", suit = "Clubs"),
+            Card(rank = "Queen", suit = "Clubs"),
+            Card(rank = "King", suit = "Clubs"),
+            Card(rank = "Ace", suit = "Clubs")
+        ]    
+        hand = Hand(cards = cards)
+        self.assertEqual(
+            hand.best_rank(),
+            "Royal Flush"
+        )
 
 
 
