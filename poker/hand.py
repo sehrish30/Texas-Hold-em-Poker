7574b1845d35dac25b1e62aec721d5bba91751f1
Sehrishah <sehrishwaheed98@gmail.com>
2020-11-14 21:41:11 +0300

    Final

--- poker/hand.py
+++ poker/hand.py
@@ -53,28 +53,6 @@ class Hand():
             if validator.is_valid():
                 return (index, validator.name, validator.valid_cards())
 
-
-            # destructure the tuple and invoke the method at tuple item 2
-            # name, validator_func = rank
-            # if validator_func():
-            #     return name      
-
-    # @property
-    # def _rank_validation_from_best_to_worst(self):
-    #     return (
-    #         ("Royal Flush", RoyalFlushValidator(cards = self.cards).is_valid),
-    #         ("Straight Flush", StraightFlushValidator(cards = self.cards).is_valid),
-    #         ("Four of a Kind",FourOfAKindValidator(cards = self.cards).is_valid),
-    #         ("Full House", FullHouseValidator(cards = self.cards).is_valid),
-    #         ("Flush", FlushValidator(cards = self.cards).is_valid),
-    #         ("Straight", StraightValidator(cards = self.cards).is_valid),
-    #         ("Three of a Kind", ThreeOfAKindValidator(cards = self.cards).is_valid),
-    #         ("Two Pair", TwoPairValidator(cards = self.cards).is_valid),
-    #         ("Pair", PairValidator(cards = self.cards).is_valid),
-    #         ("High Card", HighCardValidator(cards = self.cards).is_valid),
-    #         ("No Cards", NoCardsValidator(cards = self.cards).is_valid)
-    #     )    
-
       
 
 
